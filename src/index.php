4e150dd51d20a3ee777e43bd31a100e41c4cf366
bijinbjames <bijinbjames@gmail.com>
2023-04-17 19:03:15 +0530

    v1.2.1 section 3 pending

--- src/index.php
+++ src/index.php
@@ -118,196 +118,51 @@ while($row = mysqli_fetch_array($result))
             All Released <span class="span">Games</span>
           </h2>
 
-          <ul class="has-scrollbar">
-
-            <li class="scrollbar-item">
-              <div class="featured-game-card">
-
-                <figure class="card-banner img-holder" style="--width: 450; --height: 600;">
-                  <img src="./assets/images/featured-game-1.jpg" width="450" height="600" loading="lazy"
-                    alt="Just for Gamers" class="img-cover">
-                </figure>
-
-                <div class="card-content">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title" tabindex="-1">
-                      Just for <span class="span">Gamers</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-                <div class="card-content-overlay">
-
-                  <img src="./assets/images/featured-game-icon.png" width="36" height="61" loading="lazy" alt=""
-                    class="card-icon">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title">
-                      Just for <span class="span">Gamers</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-              </div>
-            </li>
-
-            <li class="scrollbar-item">
-              <div class="featured-game-card">
-
-                <figure class="card-banner img-holder" style="--width: 450; --height: 600;">
-                  <img src="./assets/images/featured-game-2.jpg" width="450" height="600" loading="lazy"
-                    alt="Need for Speed" class="img-cover">
-                </figure>
-
-                <div class="card-content">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title" tabindex="-1">
-                      Need for <span class="span">Speed</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-                <div class="card-content-overlay">
-
-                  <img src="./assets/images/featured-game-icon.png" width="36" height="61" loading="lazy" alt=""
-                    class="card-icon">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title">
-                      Need for <span class="span">Speed</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-              </div>
-            </li>
-
-            <li class="scrollbar-item">
-              <div class="featured-game-card">
-
-                <figure class="card-banner img-holder" style="--width: 450; --height: 600;">
-                  <img src="./assets/images/featured-game-3.jpg" width="450" height="600" loading="lazy"
-                    alt="Egypt Hunting Gamers" class="img-cover">
-                </figure>
-
-                <div class="card-content">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title" tabindex="-1">
-                      Egypt Hunting <span class="span">Gamers</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-                <div class="card-content-overlay">
-
-                  <img src="./assets/images/featured-game-icon.png" width="36" height="61" loading="lazy" alt=""
-                    class="card-icon">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title">
-                      Egypt Hunting <span class="span">Gamers</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-              </div>
-            </li>
-
-            <li class="scrollbar-item">
-              <div class="featured-game-card">
-
-                <figure class="card-banner img-holder" style="--width: 450; --height: 600;">
-                  <img src="./assets/images/featured-game-4.jpg" width="450" height="600" loading="lazy"
-                    alt="Just for Gamers" class="img-cover">
-                </figure>
-
-                <div class="card-content">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title" tabindex="-1">
-                      Just for <span class="span">Gamers</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-                <div class="card-content-overlay">
-
-                  <img src="./assets/images/featured-game-icon.png" width="36" height="61" loading="lazy" alt=""
-                    class="card-icon">
-
-                  <h3 class="h3">
-                    <a href="#" class="card-title">
-                      Just for <span class="span">Gamers</span>
-                    </a>
-                  </h3>
-
-                  <span class="card-meta">
-                    <ion-icon name="notifications"></ion-icon>
-
-                    <span class="span">Playstation 5, Xbox</span>
-                  </span>
-
-                </div>
-
-              </div>
-            </li>
-
-          </ul>
-
         </div>
       </section>
+
+      <div id="slider">
+    <input type="radio" name="slider" id="slide1" checked>
+    <input type="radio" name="slider" id="slide2">
+    <input type="radio" name="slider" id="slide3">
+    <input type="radio" name="slider" id="slide4">
+    <div id="slides">
+      <div id="overflow">
+        <div class="inner">
+          <div class="slide slide_1">
+            <div class="slide-content">
+              <h2>Slide 1</h2>
+              <p>Content for Slide 1</p>
+            </div>
+          </div>
+          <div class="slide slide_2">
+            <div class="slide-content">
+              <h2>Slide 2</h2>
+              <p>Content for Slide 2</p>
+            </div>
+          </div>
+          <div class="slide slide_3">
+            <div class="slide-content">
+              <h2>Slide 3</h2>
+              <p>Content for Slide 3</p>
+            </div>
+          </div>
+          <div class="slide slide_4">
+            <div class="slide-content">
+              <h2>Slide 4</h2>
+              <p>Content for Slide 4</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="bullets">
+      <label for="slide1"></label>
+      <label for="slide2"></label>
+      <label for="slide3"></label>
+      <label for="slide4"></label>
+    </div>
+  </div>
 
 
 
